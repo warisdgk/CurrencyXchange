c94a3fb551e8b49dc462e2f68f3366f6fc477f66
mwaris <warisdgk@gmail.com>
2023-10-18 00:01:44 +0500

    chore: moved dependencies to version catalog

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,9 +1,12 @@
+import org.gradle.kotlin.dsl.*
 import java.io.FileInputStream
 import java.util.*
 
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    id("com.google.dagger.hilt.android")
+    kotlin("kapt")
 }
 
 val keystoreProperties = Properties().apply {
@@ -76,19 +79,16 @@ android {
 
             signingConfig = signingConfigs.getByName("release")
 
-            manifestPlaceholders["enableCrashReporting"] = "true"
-            manifestPlaceholders["enableLogging"] = "false"
-
             isDebuggable = false
         }
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_18
+        targetCompatibility = JavaVersion.VERSION_18
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "18"
     }
     buildFeatures {
         compose = true
@@ -106,19 +106,54 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation("androidx.activity:activity-compose:1.7.2")
-    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.ui:ui-graphics")
-    implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-    debugImplementation("androidx.compose.ui:ui-tooling")
-    debugImplementation("androidx.compose.ui:ui-test-manifest")
+    //Base
+    implementation(libs.androidx.ktx)
+    implementation(libs.androidx.ktx.lifecycle)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.ui.tooling.preview)
+    implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.hilt.navigation.compose)
+    implementation(libs.androidx.lifecycle.runtimeCompose)
+
+    //Testing
+    testImplementation(libs.junit4)
+    androidTestImplementation(libs.junit4)
+    androidTestImplementation(libs.androidx.test.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.compose.ui.test)
+    debugImplementation(libs.androidx.compose.ui.tooling)
+    debugImplementation(libs.androidx.compose.ui.testManifest)
+
+    //Room For Local DB
+    implementation(libs.room.ktx)
+    implementation(libs.room.runtime)
+    //noinspection KaptUsageInsteadOfKsp As compiler is have issue with latest gradle plugin
+    kapt(libs.room.compiler)
+
+    //Work Manager x Hilt
+    implementation(libs.androidx.work.ktx)
+    kapt(libs.hilt.ext.compiler)
+    implementation(libs.hilt.ext.work)
+
+    //Chucker For Network Testing
+    implementation(libs.chucker)
+    debugImplementation(libs.chucker)
+    releaseImplementation(libs.chucker)
+
+    //Hilt For DI
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.compiler)
+
+    // Retrofit & Okhttp
+    implementation(libs.retrofit.core)
+    implementation(libs.retrofit.converter.scalars)
+    implementation(libs.hilt.gson.converter)
+    implementation(libs.okhttp)
+    implementation(libs.okhttp.logging.interceptor)
+}
+
+kapt {
+    correctErrorTypes = true
 }
